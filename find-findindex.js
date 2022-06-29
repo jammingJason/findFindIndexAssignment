39aee07ce48c6320a26f32677b69202566089794
Van Jason Crews <vanjasoncrews@gmail.com>
2022-06-29 15:35:20 -0400

    completed assignment

--- find-findindex.js
+++ find-findindex.js
@@ -11,11 +11,19 @@ findUserByUsername(users, 'mlewis') // {username: 'mlewis'}
 findUserByUsername(users, 'taco') // undefined
 */
 
+const users = [ { username: 'mlewis' }, { username: 'akagen' }, { username: 'msmith' } ];
 
-function findUserByUsername(usersArray, username) {}
+function findUserByUsername(usersArray, username) {
+	return usersArray.find(function(user, i) {
+		if (user.username === username) {
+			return usersArray[i];
+		}
+	});
+}
 
 /*
-Write a function called `removeUser` which accepts an array of objects, each with a key of username, and a string. The function should remove the object from the array. If the object is not found, return undefined. 
+Write a function called `removeUser` which accepts an array of objects, each with a key of username, and a string. 
+The function should remove the object from the array. If the object is not found, return undefined. 
 
 const users = [
   {username: 'mlewis'},
@@ -27,4 +35,10 @@ removeUser(users, 'akagen') // {username: 'akagen'}
 removeUser(users, 'akagen') // undefined
 */
 
-function removeUser(usersArray, username) {}
+function removeUser(usersArray, username) {
+	return usersArray.find(function(user, i) {
+		if (user.username === username) {
+			return usersArray.splice(i, 1);
+		}
+	});
+}
